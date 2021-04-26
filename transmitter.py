e08bdea68bd3440464bc8596becf29d3b6c2984d
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-04-26 12:53:45 +0530

    padding messages

--- transmitter.py
+++ transmitter.py
@@ -1,17 +1,41 @@
 import socket
 import pickle
 import sys
+import numpy as np
+from walsh_gen import walsh_generator_matrix
 
-def text2binary(message):
+class UserPair():
+    def __init__(self, uId, code):
+        self.uId = uId
+        self.code = np.reshape(code, (1, -1))
 
-	mes2bin = ''.join(format(ord(char), 'b') for char in message)
-	return mes2bin
 
-def cdma():
+def text2binarr(message):
 
-	# TODO not yet implemeted
+	mes2bin = "".join(f"{ord(char):08b}" for char in message)
+	bin2arr = np.array([[int(i) for i in mes2bin]], dtype=int)
+	return bin2arr
 
-	pass
+def cdma(no_of_users, messages):
+
+	userPairs = []
+
+	# Find longest message
+	max_len = len(max(messages, key=len))
+	print(max_len)
+	equilen_messages = [msg.ljust(max_len, ' ') for msg in messages]
+
+	walsh = np.array(walsh_generator_matrix([[-1]], no_of_users), dtype=int)
+	
+	symbols = map(text2binarr, equilen_messages)
+	for symbol in symbols:
+		print(symbol)
+		print(symbol.shape)
+
+	channel = np.zeros((max_len, walsh.shape[0]), dtype=int)
+
+
+	return "10000010011" # temp
 
 if __name__ == "__main__":	
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,7 +52,6 @@ if __name__ == "__main__":
 		messages.append(cur_message)
 
 	print("\nNO_OF_USERS: {0}\nMESSAGES: {1}".format(no_of_users, messages))
-	data = text2binary(messages[0])
-	print(data)
+	data = cdma(no_of_users, messages)
 	sock.sendall(pickle.dumps(data))
 	sock.close()	
